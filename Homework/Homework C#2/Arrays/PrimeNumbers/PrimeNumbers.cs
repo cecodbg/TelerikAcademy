6a803c7805e969b9697bba683ce3e2a6ec376c28
cecodbg <cecodbg@gmail.com>
2015-02-12 11:46:45 +0200

    new home work

---
+++ Homework/Homework C#2/Arrays/PrimeNumbers/PrimeNumbers.cs
@@ -0,0 +1,39 @@
+﻿// Write a program that finds all prime numbers in the range [1...10 000 000]. Use the Sieve of Eratosthenes algorithm.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 120;
+            bool[] primeNumbers = new bool[n];
+
+            for (int i = 2; i < Math.Sqrt(n); i++)
+            {
+                if (primeNumbers[i] == false)
+                {
+                    for (int j = i * i; j < primeNumbers.Length; j += i)
+                    {
+                        primeNumbers[j] = true;
+                    }
+                }
+            }
+
+            for (int i = 2; i < primeNumbers.Length; i++)
+            {
+                if (!primeNumbers[i])
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Homework/Homework C#2/Arrays/QuickSort/QuickSort.cs
@@ -0,0 +1,77 @@
+﻿// Write a program that sorts an array of integers using the Quick sort algorithm.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class QuickSort
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eneter numbers, separated by a comma: ");
+            int[] numbers = Console.ReadLine()
+                .Split(new char[] { ' ', ',', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            SortArray(numbers, 0, numbers.Length - 1);
+
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+
+        private static void SortArray(int[] numbers, int left, int right)
+        {
+            int pivot;
+            if (left < right)
+            {
+                pivot = Partition(numbers, left, right);
+
+                if (pivot > 1)
+                {
+                    SortArray(numbers, left, pivot - 1);
+                }
+
+                if (pivot + 1 < right)
+                {
+                    SortArray(numbers, pivot + 1, right);
+                }
+            }
+        }
+
+        private static int Partition(int[] numbers, int left, int right)
+        {
+            int pivot = numbers[left];
+
+            while (true)
+            {
+                while (numbers[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (numbers[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (numbers[right] == numbers[left] && numbers[left] == pivot)
+                    ++left;
+
+                if (left < right)
+                {
+                    numbers[left] ^= numbers[right];
+                    numbers[right] ^= numbers[left];
+                    numbers[left] ^= numbers[right];
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+    }
+}
