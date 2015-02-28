e91719d0a48008cdc1c92be293b340e9c4323f28
cecodbg <cecodbg@gmail.com>
2015-02-28 12:56:47 +0200

    Added txt.

--- Homework/Homework C#2/StringsAndTextProcessing/StringsInCSharp/StringsInCSharp.cs
+++ Homework/Homework C#2/StringsAndTextProcessing/StringsInCSharp/StringsInCSharp.cs
@@ -2,21 +2,30 @@
 // What is typical for the string data type?
 // Describe the most important methods of the String class.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace StringsInCSharp
-{
-    class StringsInCSharp
-    {
-        static void Main()
-        {
-            string str = string.Empty;
-            
-                
-        }
-    }
-}
+
+
+/*
+-Describe the strings in C#.
+    A string is an object of type String whose value is text. Internally, the text is stored as a sequential read-only collection of Char objects.
+    There is no null-terminating character at the end of a C# string; therefore a C# string can contain any number of embedded null characters ('\0').
+
+-What is typical for the string data type?
+    The String data type is reference type, which represents a sequence of Unicode characters and maps to System.String.
+    The default value of the String type is a null reference.
+    Strings are stored in the dynamic memory (managed heap). They have fixed length (String.Length),
+    elements can be accessed directly by index in the range [0...Length-1].
+
+
+-Describe the most important methods of the String class.
+    String.Length - Gets the lenght of the string
+    String.IndexOf()
+    String.Join()
+    String.Split()
+    Sting.Trim()
+    String.ToUpper()
+    String.ToLower()
+    String.CompareTo()
+    String.LastIndexOf()
+ */
+
